dd7138a89860464890f89072f853e60189fa23bb
hac6326 <120247137+hac6326@users.noreply.github.com>
2024-10-25 10:54:03 +0530

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-# app/models.py
-
-from motor.motor_asyncio import AsyncIOMotorClient
-from .config import settings
-
-client = AsyncIOMotorClient(settings.MONGO_URI)
-database = client[settings.DATABASE_NAME]
-blog_posts_collection = database["blog_posts"]
