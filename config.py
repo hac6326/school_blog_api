415b91b0f6d46eb92df255c5543b1a23ffbe31f7
hac6326 <120247137+hac6326@users.noreply.github.com>
2024-10-25 10:53:37 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# app/config.py
-
-from pydantic import BaseSettings
-
-class Settings(BaseSettings):
-    MONGO_URI: str = "mongodb://localhost:27017"
-    DATABASE_NAME: str = "school_blog_db"
-
-settings = Settings()
